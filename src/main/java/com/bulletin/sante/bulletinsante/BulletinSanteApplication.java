34b2aa6842289cf6574e1f0de3d3e1b1d00b2192
Mouhamed NDOYE <ndoyeahmed2602@gmail.com>
2019-06-22 23:26:10 +0000

    Initial commit

---
+++ src/main/java/com/bulletin/sante/bulletinsante/BulletinSanteApplication.java
@@ -0,0 +1,13 @@
+package com.bulletin.sante.bulletinsante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulletinSanteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BulletinSanteApplication.class, args);
+    }
+
+}
